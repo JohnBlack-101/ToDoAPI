9e0c5f3eccd5d06a9cca3721f6f4427a973891e3
John Black <john.m.black.16@gmail.com>
2023-11-13 16:09:42 -0600

    Completed the Steps from The Class Solution

---
+++ ToDoAPI/Controllers/ToDoesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Humanizer.Localisation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoesController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoesController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDos()
+        {
+          if (_context.ToDos == null)
+          {
+              return NotFound();
+            }
+
+            var todoes = await _context.ToDos.Include("Category").Select(x => new ToDo()
+            {
+                //Assign each resource in  our data set to a new Resource object for our application
+                ToDoId = x.ToDoId,
+                Name = x.Name,
+                CategoryId = x.CategoryId,
+                Category = x.Category != null ? new Category()
+                {
+                    CategoryId = x.Category.CategoryId
+                } : null
+            }).ToListAsync();
+
+            return Ok(todoes);
+        }
+
+        // GET: api/ToDoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDo>> GetToDo(int id)
+        {
+          if (_context.ToDos == null)
+          {
+              return NotFound();
+          }
+
+            var todoes = await _context.ToDos.Where(x => x.ToDoId == id).Select(x => new ToDo()
+            {
+                //Assign each resource in  our data set to a new Resource object for our application
+                ToDoId = x.ToDoId,
+                Name = x.Name,
+                CategoryId = x.CategoryId,
+                Category = x.Category != null ? new Category()
+                {
+                    CategoryId = x.Category.CategoryId
+                } : null
+            }).FirstOrDefaultAsync();
+
+            if (todoes == null)
+            {
+                return NotFound();
+            }
+
+            return todoes; 
+        }
+
+        // PUT: api/ToDoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDo(int id, ToDo toDo)
+        {
+            if (id != toDo.ToDoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDo>> PostToDo(ToDo toDo)
+        {
+          if (_context.ToDos == null)
+          {
+              return Problem("Entity set 'ToDoContext.ToDos'  is null.");
+          }
+            //Step 09) Modify the code below to manage how a Resource is posted
+            ToDo newToDo = new ToDo()
+            {
+                Name = toDo.Name,
+                CategoryId = toDo.CategoryId
+            };
+
+            _context.ToDos.Add(toDo);
+            await _context.SaveChangesAsync();
+
+            return Ok(newToDo);
+        }
+
+        // DELETE: api/ToDoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(int id)
+        {
+            if (_context.ToDos == null)
+            {
+                return NotFound();
+            }
+            var toDo = await _context.ToDos.FindAsync(id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDos.Remove(toDo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return (_context.ToDos?.Any(e => e.ToDoId == id)).GetValueOrDefault();
+        }
+    }
+}
--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,3 +1,6 @@
+//Step 03) Add the using statement below
+using Microsoft.EntityFrameworkCore;
+
 namespace ToDoAPI
 {
     public class Program
@@ -7,11 +10,28 @@ namespace ToDoAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.WithOrigins("OriginPolicy", "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();//"http://johnblackcoding.com"
+                });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //Step 04) Add the ResourceContext Service 
+            //This will initialize the database connection to be used in the controllers
+            builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(
+                options =>
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoDB"));
+                    //The string above should match the connectionString name in appsettings.json
+                }
+              );
 
             var app = builder.Build();
 
@@ -28,6 +48,8 @@ namespace ToDoAPI
 
 
             app.MapControllers();
+
+            app.UseCors();    
 
             app.Run();
         }
